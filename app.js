e01fd5f8fac9fde280b2ce978b162030b2931592
denfasyah <denfasyah@gmail.com>
2024-01-19 18:00:46 +0700

    create model place

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+
+
+// models
+const Place = require('./models/place');
+
+// connect mongo
+mongoose.connect('mongodb://127.0.0.1/denpiw')
+    .then((result) => {
+        console.log('connected successfully');
+    }).catch((err) => {
+        console.log(err);
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +22,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+app.get('/seed/place', async (req, res) => {
+    const place = new Place({
+        title: 'Empire State Building',
+        price : '$999',
+        description: 'A great building',
+        location: 'New York, NY',
+    })
+    await place.save();
+    res.send(place);
+
+
+})
 
 
 app.listen(2003,()=> {
